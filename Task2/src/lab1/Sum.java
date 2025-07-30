0e13efa47cf9f9e976fdf43e0f511c82a7175ed1
srujan6101 <srujanmacharla2007@gmail.com>
2025-07-30 10:59:43 +0530

    Add files via upload

---
+++ Task2/src/lab1/Sum.java
@@ -0,0 +1,13 @@
+package lab1;
+
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20,c;
+		c=a+b;
+		System.out.println("Sum:"+c);
+	}
+
+}
